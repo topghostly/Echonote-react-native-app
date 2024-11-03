8bfeb33c51faf573da4992542a4027084419c481
david <topghostly@gmail.com>
2024-11-03 15:47:51 +0000

    Starting the index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useMemo } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ColorProvider } from "@/context/ColorProvider";
 import { useColor } from "@/context/ColorProvider";
 
 // Typescript interfaces and types import
@@ -10,24 +9,52 @@ const index = () => {
   // Collect color values from context
   const { colors, theme, toggleTheme } = useColor();
 
-  // Using memo to avoid recalculating styles on rerender
-  const styles: any = useMemo(() => {
-    dynamicStyles(colors);
-  }, [colors]);
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>{colors.text}</Text>
+    <SafeAreaView
+      style={{
+        backgroundColor: colors.backgroundColor,
+        flex: 1,
+        paddingHorizontal: 10,
+      }}
+    >
+      <ScrollView style={styles.scrollHolder}>
+        <View style={styles.headerHolder}>
+          <View style={styles.userNameHolder}>
+            <Text
+              style={{
+                color: colors.text,
+                fontFamily: "Causten-Bold",
+                fontSize: 65,
+              }}
+            >
+              Beaver
+            </Text>
+          </View>
+          <View
+            style={{
+              width: 65,
+              aspectRatio: 1,
+              backgroundColor: colors.text,
+              borderRadius: 200,
+              borderCurve: "continuous",
+            }}
+          ></View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 export default index;
 
-const dynamicStyles = (colors: colorScheme) => {
-  StyleSheet.create({
-    container: {
-      backgroundColor: colors.backgroundColor,
-    },
-  });
-};
+const styles = StyleSheet.create({
+  headerHolder: {
+    width: "100%",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  scrollHolder: {
+    paddingTop: 20,
+  },
+  userNameHolder: {},
+});
